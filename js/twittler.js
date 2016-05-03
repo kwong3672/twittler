87ac73b48bffe589c45a34fbb61f10dafb43e0fa
Keith Wong <keith.wong3672@gmail.com>
2016-05-02 20:52:39 -0700

    Show time created in a human friendly format

--- js/twittler.js
+++ js/twittler.js
@@ -14,11 +14,7 @@ $(document).ready(function(){
       currentUser = newUser;
 
 
-        if (newUser === 'all'){
-          newUser = streams[newUser];
-        } else {
-          newUser = streams.users[newUser];
-        }
+
         lastPrinted = 0;
       printNew(newUser, 0);
     }
@@ -28,13 +24,20 @@ $(document).ready(function(){
 
   //prints all new tweets from given user
   var printNew = function(user, idx){
+
+    if (user === 'all'){
+      user = streams[user];     
+    } else {
+      user = streams.users[user];
+    }
+
     if (lastPrinted === idx && user.length > idx) {
       for (idx; idx < user.length; idx++){
         var tweet = user[idx];
         var $tweet = $('<div class="tweet col-sm-12" id="loc' + idx + '""></div>');
         var imgLocation = '#loc' + idx;
         var $img = $('<img src="img/' + tweet.user + '.jpg" class="img-small">')
-        $tweet.text('@' + tweet.user + ': ' + tweet.message + ': ' + tweet.created_at);
+        $tweet.text('@' + tweet.user + ': ' + tweet.message + ': ' + /*tweet.created_at*/jQuery.timeago(tweet.created_at));
 //      $tweet.prependTo($content);
         $content.prepend($tweet);
         $(imgLocation).prepend($img);
@@ -43,7 +46,7 @@ $(document).ready(function(){
     }
 
     (function(){
-      setTimeout(function(){printNew(user, idx)}, 0);
+      setTimeout(function(){printNew(currentUser, idx)}, 0);
     })()
 
 //    checkNewTweets(tweet.user, i);
@@ -51,5 +54,5 @@ $(document).ready(function(){
 
   $('.portfolio-item').on('click', changeUser);
 
-  printNew(streams.all, 0)
+  printNew('all', 0)
 });
